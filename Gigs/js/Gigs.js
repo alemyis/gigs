19aa8404ca1c741265485e1193b607a5042aaf58
Alemeshet Alemu <alemyis at gmail>
2012-11-11 18:24:24 -0800

    Starter html gigs view page

---
+++ Gigs/js/Gigs.js
@@ -0,0 +1,45 @@
+﻿
+$(document).ready(function () {
+    // setup handlers
+    WireUp();
+    LoadGig(1);
+});
+
+function WireUp() {
+}
+
+function LoadGig(gigId) {
+    var gig;
+    var url = "gigs.svc/Abouts(" + gigId +")?$expand=Positions/Notes,Extras,Trainings";
+    OData.read(
+        {
+            // fetch all data about the gig
+            requestUri: url
+        },
+        function (data, response) {
+            gig = data;
+
+            $('#about').text(gig.FirstName + ' ' + gig.LastName + ' ' + gig.Phone + ' ' + gig.Email);
+
+            $.each(gig.Positions, function () {
+                $('<p>' + this.Title + '</p>').appendTo('#positions');
+            });
+
+            $.each(gig.Trainings, function () {
+                $('<p>' + this.Degree + '</p>').appendTo('#trainings');
+            });
+
+
+            $.each(gig.Extras, function () {
+                $('<p>' + this.Blurb + '</p>').appendTo('#extras');
+            });
+            
+        },
+        function (error) {
+            LogError(error);
+        });
+}
+
+function LogError(message) {
+    alert(message);
+}
