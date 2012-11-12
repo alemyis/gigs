a989d799adcdea8201941e40722c9fa8d5da4d99
Alemeshet Alemu <alemyis at gmail>
2012-11-11 17:42:16 -0800

    Browser based Test console
    /TestConsole.html
    
    <option value="" selected>Resources</option>
    <option
    value="Abouts(1)?$expand=Positions/Notes,Extras,Trainings">Everything
    About 1</option>
    <option value="Abouts(1)">About 1</option>
    <option value="Traingins">All Trainings</option>
    <option value="Positions">All Positions</option>
    <option value="Positions/?$expand=Notes">All Positions & Notes</option>
    <option value="Extras">All Extras</option>
    <option value="Extras/?$top=2">The first two extra</option>
    <option value="Positions?$filter=year(From) le 2008 and year(To) ge
    2008">Positions held 2008</option>
    <option value="Abouts?$select=FirstName">First Name</option>

---
+++ Gigs/js/TestConsole.js
@@ -0,0 +1,20 @@
+﻿
+$(document).ready(function () {
+    // setup handlers
+    WireUp();
+});
+
+function WireUp() {
+    $('#testButton').click(LoadSelectedTest);
+}
+
+
+function LoadSelectedTest() {
+    var url = "gigs.svc/" + $('#testSelection').val();
+    $('#urlSpan').text(url);
+    $('#resultFrame').attr('src', url);
+}
+
+function LogError(message) {
+    alert(message);
+}
